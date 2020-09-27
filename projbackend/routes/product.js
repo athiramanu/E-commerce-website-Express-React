3c89f2b5d306ca4ee0919cc694001166a9f2edcf
Athira Manu <athiramanu9400@gmail.com>
2020-09-27 18:12:23 +0530

    Create update and delete products

---
+++ projbackend/routes/product.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const { 
+    getProductById, 
+    getProduct, 
+    createProduct, 
+    photo,
+    deleteProduct,
+    updateProduct,
+    getAllProducts
+ } = require('../controllers/product');
+const { isSignedIn, isAdmin, isAuthenticated } = require('../controllers/auth');
+const { getUserById } = require('../controllers/user');
+
+//params
+router.param('userId', getUserById);
+router.param('productId', getProductById);
+
+//actual routes
+
+//read
+router.get('/product/:productId', getProduct);
+router.get('/product/photo/:productId', photo);
+
+//create
+router.post('/product/create/:userId', isSignedIn, isAuthenticated, isAdmin, createProduct);
+
+//remove
+router.delete('/product/delete/:productId/:userId', isSignedIn, isAuthenticated, isAdmin, deleteProduct);
+
+//update
+router.put('/product/delete/:productId/:userId', isSignedIn, isAuthenticated, isAdmin, updateProduct);
+
+//listing 
+router.get('/products', getAllProducts);
+
+module.exports = router;
