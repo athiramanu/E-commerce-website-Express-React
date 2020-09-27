eb1feb32766f5ef477c443868688a73a8354ee49
Athira Manu <athiramanu9400@gmail.com>
2020-09-27 11:22:32 +0530

    Ecommerce backend

---
+++ projbackend/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+//Routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+
+//DB connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(function(){
+    console.log("DB CONNECTED")
+});
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Start a server
+app.listen(port, function(){
+    console.log(`App is running at ${port}`);
+});
