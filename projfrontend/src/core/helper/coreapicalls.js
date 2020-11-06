59814250723fcc87a2df5c24e8072b53d6bf7b4d
Athira Manu <athiramanu9400@gmail.com>
2020-11-06 09:53:34 +0530

    Products displayed as cards

---
+++ projfrontend/src/core/helper/coreapicalls.js
@@ -0,0 +1,11 @@
+import { API } from '../../backend';
+
+export const getProducts = () => {
+    return fetch(`${API}/products`, {
+        mathod: "GET"
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err));
+}
