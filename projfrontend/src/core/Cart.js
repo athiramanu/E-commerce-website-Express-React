eaa046288f3af4fc457dfdb7bb4895c7bb6a90b5
Athira Manu <athiramanu9400@gmail.com>
2021-01-16 10:59:18 +0530

    Payment gateway backend setup

--- projfrontend/src/core/Cart.js
+++ projfrontend/src/core/Cart.js
@@ -4,6 +4,7 @@ import { API } from '../backend';
 import Base from './Base';
 import Card from './Card';
 import { loadCart } from './helper/cartHelper';
+import StripeCheckout from './StripeCheckout';
 
 const Cart = () => {
     const [products, setProducts] = useState([]);
@@ -34,7 +35,11 @@ const Cart = () => {
     const loadCheckout = () => {
         return (
             <div>
-                <h2>This section is for checkout</h2>
+                <StripeCheckout
+                    products={products}
+                    setReload={setReload}
+                    reload={reload}
+                />
             </div>
         )
     }
